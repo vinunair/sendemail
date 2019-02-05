4424465c3e20037150568c2009c129a45e1f9174
Nair, Vinu | RASIA <vinu.nair@rakuten.com>
2019-02-05 11:48:31 +0800

    Setup pom for spring boot

---
+++ src/main/java/com/github/vinunair/sendemail/App.java
@@ -0,0 +1,13 @@
+package com.github.vinunair.sendemail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
